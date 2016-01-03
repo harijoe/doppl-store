ee5c4d6a0b75f7a5a271622e2766be5930989c1b
Julien Vallini <vallinij@gmail.com>
2016-01-03 20:42:11 +0100

    Create WeatherMeasure entity

---
+++ src/AppBundle/Entity/WeatherMeasure.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A weather measure.
+ * 
+ * @ORM\Entity
+ */
+class WeatherMeasure
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var \DateTime Date of birth.
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     * @Assert\DateTime
+     */
+    private $datetimeCreate;
+    /**
+     * @var int temperature
+     *
+     * @ORM\Column(type="decimal", nullable=true, scale=2)
+     * @Assert\Type(type="float")
+     */
+    private $temparature;
+    /**
+     * @var int Luminance
+     *
+     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Type(type="integer")
+     */
+    private $luminance;
+    /**
+     * @var int The number of pages in the book.
+     *
+     * @ORM\Column(type="decimal", nullable=true, scale=2)
+     * @Assert\Type(type="float")
+     */
+    private $humidity;
+
+    public function __construct()
+    {
+        $this->datetimeCreate = new \DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDatetimeCreate()
+    {
+        return $this->datetimeCreate;
+    }
+
+    /**
+     * @param \DateTime $datetimeCreate
+     */
+    public function setDatetimeCreate($datetimeCreate)
+    {
+        $this->datetimeCreate = $datetimeCreate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTemparature()
+    {
+        return $this->temparature;
+    }
+
+    /**
+     * @param int $temparature
+     */
+    public function setTemparature($temparature)
+    {
+        $this->temparature = $temparature;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLuminance()
+    {
+        return $this->luminance;
+    }
+
+    /**
+     * @param int $luminance
+     */
+    public function setLuminance($luminance)
+    {
+        $this->luminance = $luminance;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHumidity()
+    {
+        return $this->humidity;
+    }
+
+    /**
+     * @param int $humidity
+     */
+    public function setHumidity($humidity)
+    {
+        $this->humidity = $humidity;
+    }
+}

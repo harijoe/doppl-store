59dc70132fd0a025f046c08d5957c8614a715254
Julien Vallini <vallinij@gmail.com>
2016-01-04 22:17:21 +0100

    Iterate and retrieve stars

--- src/AppBundle/Command/FetchGithubStarsCommand.php
+++ src/AppBundle/Command/FetchGithubStarsCommand.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Command;
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -23,6 +24,17 @@ class FetchGithubStarsCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $client = new Client(['base_url' => 'http://localhost']);
+//        $repositories = $client->request('GET', 'http://localhost/watched_repositories?_format=json');
+        $response = $client->get('http://localhost/watched_repositories?_format=json');
+        $repositories = json_decode($response->getBody()->getContents(), true)['hydra:member'];
 
+        foreach ($repositories as $repository) {
+            $response = $client->get('https://api.github.com/repos/'.$repository['name']);
+            $res = $client->get('https://api.github.com/repos/'.$repository['name']);
+            $stars = json_decode($response->getBody()->getContents(), true)['stargazers_count'];
+            var_dump($stars);
+        }
+//            'auth' => ['user', 'pass']
     }
 }
